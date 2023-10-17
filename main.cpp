d2d6e22ef45cbc48b409811f98e3b5e146ba71d7
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-17 20:15:41 +0300

    ex-2 was done

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
+#include <Windows.h>
 #include <cmath>
 
 using namespace std;
 
 int main() {
+	system("chcp 1251");
+	system("cls");
 	float l, r, s;
-
 	cout << "¬вед≥ть довжину кола: ";
 	cin >> l;
-
-	r = l / (2 * M_PI);
-
-	s = M_PI * pow(r, 2);
-
+	r = l / (2 * 3,14);
+	s = 3,14 * pow(r, 2);
 	cout << "ѕлоща кола: " << s << endl;
-
 	return 0;
 }
